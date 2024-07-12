325cf1ebf02567940f3108082fff31f4d23f1087
marirhs <marirhs@gmail.com>
2024-07-12 10:53:12 -0400

    change the query to prod schema

--- sql/marirhs_dw/staging/csv_column_to_rows.sql.sql
+++ sql/marirhs_dw/staging/csv_column_to_rows.sql.sql
@@ -1,26 +1,25 @@
 	
 -- convert csv column to rows.  The CTE does that and then the outer query total ingredients per pizza
 		with pizzas as
-			( 
-				select 
-					distinct
-					pizza_type_id
-					,"name" as pizza_name
-					,category
-					,trim(unnest(string_to_array(ingredients,','))) individual_ingredients
-					,trim(string_to_table(ingredients,',')) as individual_ingredients_2
-			from
-			staging.pizza_types
-			order by 1
-			)
+		( 
 			select 
-				pizza_type_id
+				distinct
+				pizza_id
 				,pizza_name
-				,count(*) as total_ingredients
-			from 
-					pizzas
-			group by 
-						pizza_type_id
-						,pizza_name
-			order by 3 desc
-			;
+				,trim(unnest(string_to_array(toppings,','))) toppings
+				,trim(string_to_table(toppings,',')) as toppings_2
+			from
+			production.dim_pizza
+			order by 1
+		)
+		select 
+			pizza_id
+			,pizza_name
+			,count(*) as total_toppings
+		from 
+			pizzas
+		group by 
+			pizza_id
+			,pizza_name
+		order by 3 desc
+		;
